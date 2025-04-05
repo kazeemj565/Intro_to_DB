b7576e54f319d99faa43633d9f18a29abf98699e
kazeem jamiu <kazeemj565@gmail.com>
2025-04-05 17:30:23 +0100

    feat: add MySQLServer file

---
+++ MySQLServer.py
@@ -0,0 +1,77 @@
+import mysql.connector
+
+
+mydb = mysql.connector.connect(
+    host='localhost',
+    username='root',
+    password='Qasim@alx2024',
+    database='alx_book_store'
+)
+
+
+print(mydb.get_server_info())
+
+mycursor = mydb.cursor()
+
+mycursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
+
+mydb.commit()
+print("Database 'alx_book_store' created successfully!")
+
+
+mycursor.execute("""
+CREATE TABLE IF NOT EXISTS Books (
+    book_id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(130) NOT NULL,
+    author_id INT,
+    price DOUBLE NOT NULL,
+    publication_date DATE,
+    FOREIGN KEY (author_id) REFERENCES Authors(author_id)
+)
+""")
+
+mydb.commit()
+
+mycursor.execute("""
+CREATE TABLE IF NOT EXISTS Authors (
+    author_id INT AUTO_INCREMENT PRIMARY KEY,
+    author_name VARCHAR(215) NOT NULL
+)
+""")
+
+mycursor.execute("""
+CREATE TABLE IF NOT EXISTS Customers (
+    customer_id INT AUTO_INCREMENT PRIMARY KEY,
+    customer_name VARCHAR(215) NOT NULL,
+    email VARCHAR(215) NOT NULL,
+    address TEXT
+)
+
+""")
+
+
+mycursor.execute("""
+CREATE TABLE IF NOT EXISTS Orders (
+    order_id INT AUTO_INCREMENT PRIMARY KEY,
+    customer_id INT,
+    order_date DATE,
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
+)
+""")
+
+mycursor.execute("""
+CREATE TABLE IF NOT EXISTS Order_Details (
+    orderdetailid INT AUTO_INCREMENT PRIMARY KEY,
+    order_id INT,
+    book_id INT,
+    quantity DOUBLE NOT NULL,
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY (book_id) REFERENCES Books(book_id)
+)
+""")
+
+mydb.commit()
+
+mycursor.close()
+mydb.close()
+print
